0b3f8415f6995d53e4b53bd864e113314ea7462a
rexis <rexis4208@gmail.com>
2026-06-30 21:41:27 +0500

    добавлены функции insert_into_table и select_from_table в database.py добавлены функции append_row и is_table_exists в storage.py

--- src/database.py
+++ src/database.py
@@ -1,12 +1,31 @@
 import os
-from src.storage import save_table
+from src.storage import *
 
 
 def create_table(table_name, columns):
-    path = os.path.join("data", f"{table_name}.csv")
-
-    if os.path.exists(path):
-        print("Таблица уже существует")
+    # функция создает таблицу с заданными в список columns аргументами
+    if is_table_exists(table_name):
+        print('Таблица уже существует')
         return
 
     save_table(table_name, [columns])
+
+
+def insert_into_table(table_name, values):
+    table = load_table(table_name)
+
+    if len(values) != len(table[0]):
+        print("Неверное количество значений")
+        return
+
+    append_row(table_name, values)
+
+
+def select_from_table(table_name):
+    #Пока что работает только со всей таблицей целиком
+    table = load_table(table_name)
+
+    if table is None:
+        return None
+
+    return table
--- src/storage.py
+++ src/storage.py
@@ -14,8 +14,23 @@ def load_table(table_name):
 
 
 def save_table(table_name, data):
+    # функция изменяет и сохраняет информацию, data ожидает матрицу на вход, полностью перезаписывает таблицу
     path = os.path.join("data", f"{table_name}.csv")
 
     with open(path, 'w', newline='', encoding='UTF-8') as file:
         writer = csv.writer(file)
         writer.writerows(data)
+
+def append_row(table_name, row):
+    path = os.path.join("data", f"{table_name}.csv")
+    with open(path, 'a', newline='', encoding='UTF-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(row)
+
+
+def is_table_exists(table_name):
+    path = os.path.join("data", f"{table_name}.csv")
+    if os.path.exists(path):
+        return True
+
+    return False
